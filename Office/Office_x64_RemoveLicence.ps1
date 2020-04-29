949a1aad02e4093d5544164fff3334ccbb71837a
Julien Vavasseur <sign.github@glop.info>
2020-04-29 15:02:24 +0200

    Update Office_x64_RemoveLicence.ps1

--- Office/Office_x64_RemoveLicence.ps1
+++ Office/Office_x64_RemoveLicence.ps1
@@ -2,9 +2,6 @@ $licencepath = "${env:ProgramFiles(x86)}\Microsoft Office\root\Licenses16"
 $kmshost = "kms.srv.crsoo.com"
 $ospp = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\ospp.vbs"
 
-$licence = "XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99"
-#$licence = "MWYG8-HM3WH-JQTYR-X8JK3-GHT22"
-#$licence = "PYQN8-3BXR6-MTMH8-DH2TR-K4TDY"
 
 $(
 	write-output "----------------------------------------------------------------------------------------------------"
