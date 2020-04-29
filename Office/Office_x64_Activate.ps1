8ebe19ad0b2b160052d6517184f0ee5d912b2460
Julien Vavasseur <sign.github@glop.info>
2020-04-29 15:01:51 +0200

    Add Office Licence Scripts

---
+++ Office/Office_x64_Activate.ps1
@@ -0,0 +1,34 @@
+﻿param(
+    [Parameter(Mandatory=$false, ValueFromPipeline = $false)] [string] $LicencePath = "${env:ProgramFiles}\Microsoft Office\root\Licenses16"
+	, [Parameter(Mandatory=$false, ValueFromPipeline = $false)] [string] $KmsHost = "kms.srv.crsoo.com"
+    , [Parameter(Mandatory=$false, ValueFromPipeline = $true)] [string] $OSPP = "${env:ProgramFiles}\Microsoft Office\Office16\ospp.vbs"
+    , [Parameter(Mandatory=$true, ValueFromPipeline = $true)] [string] $Licence
+)
+
+write-host "Licence Path: $LicencePath" 
+write-host "KMS Host: $KmsHost" 
+write-host "OSPP: $OSPP" 
+
+$LicenceKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Licence))
+write-host "----------------------------------------------------------------------------------------------------" 
+
+cscript "$OSPP" /inslic:"$LicencePath\ProPlusVL_KMS_Client-ppd.xrm-ms"
+cscript "$OSPP" /inslic:"$LicencePath\ProPlusVL_KMS_Client-ul.xrm-ms"
+cscript "$OSPP" /inslic:"$LicencePath\ProPlusVL_KMS_Client-ul-oob.xrm-ms"
+cscript "$OSPP" /inslic:"$LicencePath\pkeyconfig-office.xrm-ms"
+
+write-host "----------------------------------------------------------------------------------------------------" 
+write-host "set key"
+cscript "$OSPP" /inpkey:$LicenceKey
+
+write-host "----------------------------------------------------------------------------------------------------" 
+write-host "set host"
+cscript "$OSPP" /sethst:$KmsHost
+
+write-host "----------------------------------------------------------------------------------------------------" 
+write-host "status"
+cscript "$OSPP" /dstatus
+write-host
+write-host Retail to Volume License conversion finished.
+write-host
+#pause
---
+++ Office/Office_x64_RemoveLicence.ps1
@@ -0,0 +1,18 @@
+$licencepath = "${env:ProgramFiles(x86)}\Microsoft Office\root\Licenses16"
+$kmshost = "kms.srv.crsoo.com"
+$ospp = "${env:ProgramFiles(x86)}\Microsoft Office\Office16\ospp.vbs"
+
+$licence = "XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99"
+#$licence = "MWYG8-HM3WH-JQTYR-X8JK3-GHT22"
+#$licence = "PYQN8-3BXR6-MTMH8-DH2TR-K4TDY"
+
+$(
+	write-output "----------------------------------------------------------------------------------------------------"
+	write-output "----------------------------------------------------------------------------------------------------"
+	Get-Date
+	write-output "Remove Licence"
+
+	cscript "$ospp" /unpkey:WFG99
+	#cscript "$ospp" /unpkey:BTDRB
+	write-output ""
+) >> "$env:public\licence.txt"
